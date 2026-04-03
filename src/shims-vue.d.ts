5751488bae27d69488230f8bf5d3a1ea2b3721a6
Jay <1272083926@qq.com>
2026-04-03 13:36:45 +0800

    feat 定义文件

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,6 @@
 // src/shims-vue.d.ts
-import { DefineComponent } from 'vue';
 declare module '*.vue' {
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
+	import type { DefineComponent } from 'vue'
+	const component: DefineComponent<{}, {}, any>
+	export default component
 }
